86d0811c80e59dd937a74c09c3b95a0339e14745
notsamueltjy@gmail.com <126168312+SAMTAN444@users.noreply.github.com>
2025-12-13 12:03:40 +0800

    feat: implement user authentication with JWT

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
 import connectDB from "./config/db.js"
+import authRoutes from "./routes/authRoutes.js"
 
 dotenv.config();
 connectDB();
@@ -18,6 +19,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.send("API running");
 });
+
+app.use("/api/auth", authRoutes);
 
 const PORT = 5001;
 app.listen(PORT, () => {
