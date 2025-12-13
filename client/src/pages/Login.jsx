ea1ea0ae4e293b37ab665b4bd2b7cc079489b11e
notsamueltjy@gmail.com <126168312+SAMTAN444@users.noreply.github.com>
2025-12-13 20:47:10 +0800

    add API interceptor for JSON webtoken and logo to login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,22 @@
+import React, { useState } from "react";
+import API from "../services/api";
+import { useNavigate } from "react-router-dom";
+import logo from "../assets/logo.png"
+
 export default function Login() {
-    return (
-        <>
-            Login
-        </>
-    )
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const [state, setState] = React.useState("login");
+  const [name, setName] = React.useState("");
+
+  return (
+    <>
+      <div className="bg-gray-50">
+        <div className="max-w-120 w-full">
+            <a href="javascript:void(0)"><img src={logo} alt="logo" className="w-40 mb-8 mx-auto block" /></a>
+        </div>
+      </div>
+    </>
+  );
 }
